6050c0d453c4645e8bfe751f494c446d1f12c9ef
hyson.han <hyson.han@vipshop.com>
2018-02-04 21:00:07 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hyson 2018/2/4 下午8:56
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hyson 2018/2/4 下午8:56
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Test Hello World!");
+    }
+}
